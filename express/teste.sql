0d3ef2d9909831b3ac911801a70f4efc35666835
LuizFritsch <fritsch.guilherm3@gmail.com>
2021-03-29 16:35:02 -0300

    edit projeto 1
    -aprendi a renderizar arquivos html (ejs)
    -aprendi a enviar dados para o ejs

---
+++ express/teste.sql
@@ -0,0 +1,12 @@
+/*CREATE TABLE usuarios(
+    nome VARCHAR(50),
+    email VARCHAR(100),
+    idade INT
+);*/
+
+INSERT INTO usuarios(nome,email,idade) VALUES(
+"Luiz ",
+"email@test.com",
+32
+);
+
---
+++ projeto_perguntas_respostas/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+
+//dizendo ao express q o ejs sera o view engine
+app.set('view engine', 'ejs');
+
+app.get("/:nome/:lang", function(req, res) {
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    var exibirMsg = false;
+
+    var produtos = [
+        { nome: "doritos", preco: 5 },
+        { nome: "coca-cola", preco: 7 },
+        { nome: "banana", preco: 3 },
+        { nome: "nesca", preco: 2 },
+        { nome: "cenoura", preco: 1 },
+        { nome: "cebola", preco: 0.8 },
+    ];
+
+    res.render("index", {
+        nome,
+        lang,
+        empresa: "solutio",
+        inscritos: 8000,
+        msg: exibirMsg,
+        produtos
+    });
+});
+
+app.listen(
+    8080, () => {
+        console.log("app rodando");
+    }
+);
+/*
+app.get("/", function(req, res) {
+    res.send();
+});
+*/
