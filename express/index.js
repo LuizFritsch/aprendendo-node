7f64dc9138f3b473714eb9b5b159dbe2e401f0b7
LuizFritsch <fritsch.guilherm3@gmail.com>
2021-03-29 15:40:33 -0300

    edit index
    -aprendi a utilizar parametros na url (get, query) opcionais e n opcinais e aprendi a apresentar esses parametros na tela tbm

--- express/index.js
+++ express/index.js
@@ -11,12 +11,45 @@ app.get("/", function(req, res) {
     //res.send("bem vindo de novo");
 });
 
-app.get("/blog", function(req, res) {
-    res.send("bem vindo ao meu blog");
+/**
+ * ola
+ * :nome parametro get obrigatorio na url
+ * :empresa parametro get obrigatorio na url
+ * Para pegar o que foi passado na url a gente usa:
+ * req.params.
+ * 
+ */
+app.get("/ola/:nome/:empresa", function(req, res) {
+    res.send("<h1>oi " + req.params.nome + ", da " + req.params.empresa + "</h1>")
 });
 
+/**
+ * blog
+ * :artigo? parametro get NAO obrigatorio na url
+ * 
+ */
+app.get("/blog/:artigo?", function(req, res) {
+
+    var artigo = req.params.artigo;
+
+    if (!artigo) {
+        res.send("bem vindo ao meu blog");
+    } else {
+        res.send("<h2>Artigo: </h2>" + artigo)
+    }
+
+});
+
+/**
+ * Query parameter
+ */
 app.get("/canal/youtube", function(req, res) {
-    res.send("bem vindo ao meu canal");
+    var canal = req.query["canal"];
+    if (canal) {
+        res.send(canal);
+    } else {
+        res.send("nenhum canal fornecido");
+    }
 });
 
 app.listen(3000, function(erro) {
