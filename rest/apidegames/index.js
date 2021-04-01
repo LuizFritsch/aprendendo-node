754bf373b76d8397c6754a02f5e16cb9c6633e41
LuizFritsch <fritsch.guilherm3@gmail.com>
2021-04-01 12:45:30 -0300

    add endpoint list by id
    -se acessar /game/:id ele lista o game com aquele id, se acessar com algo n numerico retorna 400 (bad request) e se acessar com um id q n tem no banco retorna 404(not found)

--- rest/apidegames/index.js
+++ rest/apidegames/index.js
@@ -36,6 +36,33 @@ app.get("/games", (req, res) => {
     res.json(db.games);
 });
 
+/**
+ * endpoint
+ * list game by id
+ */
+app.get("/game/:id", (req, res) => {
+    var id = req.params.id;
+    if (isNaN(id)) {
+        //not numeric
+        //invalid parameter
+        res.sendStatus(400);
+    } else {
+        id = parseInt(id);
+
+        var game = db.games.find(g => g.id == id);
+
+        if (game) {
+            res.json(game);
+        } else {
+            //game with id not found
+            res.sendStatus(404);
+        }
+
+    }
+    //res.statusCode = 200;
+    //res.json(db.games);
+});
+
 app.listen(80, () => {
     console.log("api rodando...");
 })
