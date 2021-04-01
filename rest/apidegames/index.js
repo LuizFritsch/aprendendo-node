4d0e365702796fc631157ac6edcbec90dbe45bf0
LuizFritsch <fritsch.guilherm3@gmail.com>
2021-04-01 13:19:59 -0300

    edit api
    -add delete
    -add put
    -add post

--- rest/apidegames/index.js
+++ rest/apidegames/index.js
@@ -50,7 +50,7 @@ app.get("/game/:id", (req, res) => {
         id = parseInt(id);
 
         var game = db.games.find(g => g.id == id);
-
+        console.log(game);
         if (game) {
             res.json(game);
         } else {
@@ -61,6 +61,79 @@ app.get("/game/:id", (req, res) => {
     }
     //res.statusCode = 200;
     //res.json(db.games);
+});
+
+/**
+ * endpoint
+ * insert game
+ */
+app.post("/game", (req, res) => {
+    var { name, price, year } = req.body;
+    console.log(req.body);
+    db.games.push({
+        id: Math.floor(Math.random() * 11),
+        name,
+        price,
+        year
+    });
+    res.sendStatus(200);
+});
+/**
+ * endpoint
+ * delete game by id
+ */
+app.delete("/game/:id", (req, res) => {
+    var id = req.params.id;
+    if (isNaN(id)) {
+        //not numeric
+        //invalid parameter
+        res.sendStatus(400);
+    } else {
+        id = parseInt(id);
+
+        var index = db.games.findIndex(g => g.id == id);
+        console.log(index);
+        if (index == -1) {
+            //game with id not found
+            res.sendStatus(404);
+        } else {
+            db.games.splice(index, 1);
+            res.sendStatus(200);
+        }
+
+    }
+});
+
+app.put("/game/:id", (req, res) => {
+    var id = req.params.id;
+    if (isNaN(id)) {
+        //not numeric
+        //invalid parameter
+        res.sendStatus(400);
+    } else {
+        id = parseInt(id);
+
+        var game = db.games.find(g => g.id == id);
+        console.log(game);
+        if (game != undefined) {
+            var { name, price, year } = req.body;
+            if (name != undefined) {
+                game.name = name;
+            }
+            if (price != undefined) {
+                game.price = price;
+            }
+            if (year != undefined) {
+                game.year = year;
+            }
+
+            res.sendStatus(200);
+        } else {
+            //game with id not found
+            res.sendStatus(404);
+        }
+
+    }
 });
 
 app.listen(80, () => {
