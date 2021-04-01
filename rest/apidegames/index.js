ae7a7250614efdbb5d276bf85811d8782d457e29
LuizFritsch <fritsch.guilherm3@gmail.com>
2021-04-01 14:44:46 -0300

    consumindo API REST utilizando AXIOS
    consumi utilizando put, post, get e delete uma api para loja de jogos

--- rest/apidegames/index.js
+++ rest/apidegames/index.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const app = express()
-
+const cors = require("cors")
 const bodyparser = require("body-parser")
+app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
