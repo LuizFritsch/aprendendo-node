7779b5f7322eea5f18de5d07410b9765df6c7545
LuizFritsch <fritsch.guilherm3@gmail.com>
2021-04-02 17:39:34 -0300

    aprendendo vue
    -conceitos de reatividade
    -v-models databiding
    -interpolacao

---
+++ vue/clientes/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>teste</h1>
+    <Cliente/>
+    <Cliente/>
+  </div>
+</template>
+
+<script>
+import Cliente from './components/Cliente'
+export default {
+  name: 'App',
+  components:{
+    Cliente
+  }
+}
+</script>
+
+<style>
+#app {
+}
+</style>
---
+++ vue/clientes/src/components/Cliente.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h2>Cliente Aqui!</h2>    
+        <h2 id="cliente-nome">{{ nome }}</h2> 
+        <hr>
+        <p>email do cliente {{email}}</p>
+        <input type="text" v-model="nome">
+        <input type="text" :value="nome">
+        <input type="text" :value="email">
+        <input type="text" v-model="email">
+        <input type="text" :value="idade">
+        <input type="text" v-model="idade">
+        <input type="text" :value="numero">
+        <input type="text" v-model="numero">
+    </div>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return {
+            nome : "luiz",
+            numero : "123",
+            email : "321",
+            idade : 0
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+hr{
+    border-color: brown;
+    border-width: 10px;
+}
+</style>
