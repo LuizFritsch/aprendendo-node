38b74de6e139f81ea5477e7012f73de132f264fb
LuizFritsch <fritsch.guilherm3@gmail.com>
2021-04-05 14:16:39 -0300

    aprendendo router no vuejs
    -como criar rota
    -estrutura rota
    -componentes em rota
    -router link
    -parametros
    -nested routes

---
+++ router/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Cadastro from '../views/Cadastro.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About
+    },
+    {
+        path: '/cadastro',
+        name: 'cadastro',
+        component: Cadastro,
+        children: [{
+                path: 'lima',
+                name: 'lima',
+                component: About,
+            }
+
+        ]
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
---
+++ router/src/views/Cadastro.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <input type="text" name="email" id="email" placeholder="email">
+      <br>
+      <input type="text" name="nome" id="nome" placeholder="nome">
+      <br>
+      <button>teste</button>
+      <h4>{{parametroRota}}</h4>
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+    created(){
+        
+    },
+    data(){
+        return{
+            parametroRota: "nada"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
