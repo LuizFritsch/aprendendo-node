a4647682384bf812c24a916ed15be58726994f78
LuizFritsch <fritsch.guilherm3@gmail.com>
2021-04-07 09:59:52 -0300

    projeto upload
    conexao com o banco

--- projeto upload_imagens/src/app.js
+++ projeto upload_imagens/src/app.js
@@ -1,7 +1,16 @@
 let express = require("express");
 let app = express()
+let mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/galeria", { useNewUrlParser: true, useUnifiedTopology: true }).then((result) => {
+    console.log("conectado com o banco...");
+}).catch((err) => {
+    console.log(err);
+});
+
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 
 app.get("/", (req, res) => {
     res.json({});
--- projeto upload_imagens/test/server.test.js
+++ projeto upload_imagens/test/server.test.js
@@ -1,7 +1,7 @@
 var supertest = require("supertest");
 let app = require("../src/app");
 let request = supertest(app);
-teste("deve rodar na porta 80", () => {
+test("deve rodar na porta 80", () => {
     return request.get("/").then(res => {
         expect(res.statusCode).toEqual(200)
     }).catch(err => {
